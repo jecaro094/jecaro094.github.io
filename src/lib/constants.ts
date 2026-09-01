596ad865235b7def36b43e295185dae2a066ca17
jecaro094 <jecarobd@gmail.com>
2026-09-01 11:10:43 +0200

    chore: Update cv

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const SOCIAL_LINKS = {
   linkedin: 'https://www.linkedin.com/in/jesus-caballero-rodriguez/',
   github: 'https://github.com/jecaro094',
+  cv_path: '/cv/Jesus_Caballero_Rodriguez_CV.pdf' 
 } as const;
